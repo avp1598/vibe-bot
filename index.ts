9821157532ae2a584bfd654409618e91bd6a5bb2
Aditya Veer Parmar <nakulparmar15@gmail.com>
2025-03-24 15:07:25 +0530

    integrate ayaos and initialize agent

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { Agent, ayaLogger } from "@tribesxyz/ayaos";
+
+async function main() {
+  const agent = new Agent({
+    dataDir: "data",
+  });
+
+  await agent.start();
+
+  ayaLogger.info("Agent started with ID:", agent.agentId);
+}
+
+main().catch(console.error);
